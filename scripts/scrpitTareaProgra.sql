da8f249e5fb9b2c63967ef5f8bf5a19c715427ea
HaroldAH <158773223+HaroldAH@users.noreply.github.com>
2025-08-06 13:06:51 -0600

    Nuevo scprit

---
+++ scripts/scrpitTareaProgra.sql
@@ -0,0 +1,148 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               11.8.2-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.10.0.7000
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for rutinasdb
+CREATE DATABASE IF NOT EXISTS `rutinasdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_ai_ci */;
+USE `rutinasdb`;
+
+-- Dumping structure for table rutinasdb.completedactivity
+CREATE TABLE IF NOT EXISTS `completedactivity` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) DEFAULT NULL,
+  `routine_id` bigint(20) DEFAULT NULL,
+  `habit_id` bigint(20) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `completedAt` varchar(10) DEFAULT NULL,
+  `isCompleted` tinyint(1) DEFAULT NULL,
+  `notes` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `routine_id` (`routine_id`),
+  KEY `habit_id` (`habit_id`),
+  CONSTRAINT `completedactivity_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `completedactivity_ibfk_2` FOREIGN KEY (`routine_id`) REFERENCES `routine` (`id`),
+  CONSTRAINT `completedactivity_ibfk_3` FOREIGN KEY (`habit_id`) REFERENCES `habitactivity` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table rutinasdb.guide
+CREATE TABLE IF NOT EXISTS `guide` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) DEFAULT NULL,
+  `content` text DEFAULT NULL,
+  `category` enum('P','M','S','D') DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table rutinasdb.guide_habit
+CREATE TABLE IF NOT EXISTS `guide_habit` (
+  `guide_id` bigint(20) NOT NULL,
+  `habit_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`guide_id`,`habit_id`),
+  KEY `habit_id` (`habit_id`),
+  CONSTRAINT `guide_habit_ibfk_1` FOREIGN KEY (`guide_id`) REFERENCES `guide` (`id`),
+  CONSTRAINT `guide_habit_ibfk_2` FOREIGN KEY (`habit_id`) REFERENCES `habitactivity` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table rutinasdb.habitactivity
+CREATE TABLE IF NOT EXISTS `habitactivity` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `category` varchar(50) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `duration` int(11) DEFAULT NULL,
+  `targetTime` time DEFAULT NULL,
+  `notes` varchar(255) DEFAULT NULL,
+  `isFavorite` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table rutinasdb.reminder
+CREATE TABLE IF NOT EXISTS `reminder` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) DEFAULT NULL,
+  `habit_id` bigint(20) DEFAULT NULL,
+  `time` varchar(10) DEFAULT NULL,
+  `frequency` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `habit_id` (`habit_id`),
+  CONSTRAINT `reminder_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `reminder_ibfk_2` FOREIGN KEY (`habit_id`) REFERENCES `habitactivity` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table rutinasdb.role
+CREATE TABLE IF NOT EXISTS `role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `permissions` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table rutinasdb.routine
+CREATE TABLE IF NOT EXISTS `routine` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  `daysOfWeek` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `routine_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table rutinasdb.user
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table rutinasdb.user_role
+CREATE TABLE IF NOT EXISTS `user_role` (
+  `user_id` bigint(20) NOT NULL,
+  `role_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `user_role_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `user_role_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- Data exporting was unselected.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
